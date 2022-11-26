45d7fc5eab1c95d643ccbeb8c0e1520c21fa036f
AXON <axonasif@gmail.com>
2022-11-26 21:48:48 +0600

    refactor: Remove one extra line

--- src/lib.sh
+++ src/lib.sh
@@ -1,7 +1,6 @@
 function bashenv.zsh() {
   if test ! -v AUTOLOAD_BASH_INJ; then
-    export AUTOLOAD_BASH_INJ=true
-    exec bash -lic 'exec zsh'
+    exec bash -lic 'AUTOLOAD_BASH_INJ=true exec zsh'
   fi
   unset AUTOLOAD_BASH_INJ
 };
