9b3bb5e06468ca9d35381354c85a7af3e7830624
AXON <axonasif@gmail.com>
2022-11-26 14:18:01 +0600

    init

---
+++ Bashbox.sh
@@ -0,0 +1,7 @@
+NAME="bashenv.zsh"
+CODENAME="bashenv.zsh"
+AUTHORS=("AXON <axonasif@gmail.com>")
+VERSION="1.0"
+DEPENDENCIES=()
+REPOSITORY=""
+BASHBOX_COMPAT="0.4.1~"
---
+++ src/lib.sh
@@ -0,0 +1,9 @@
+## BASHENV.ZSH
+function bashenv.zsh() {
+  if test ! -v AUTOLOAD_BASH_INJ; then
+    export AUTOLOAD_BASH_INJ=true
+    exec bash -lic 'exec zsh'
+  fi
+  unset AUTOLOAD_BASH_INJ
+}
+## BASHENV.ZSH
